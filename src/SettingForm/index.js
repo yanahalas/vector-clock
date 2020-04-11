816621d156aeefa0c3879c1d5224057fb8bbc11c
Yana Halas <yngalas@gmail.com>
2020-04-11 16:22:50 +0300

    Update index.js

--- src/SettingForm/index.js
+++ src/SettingForm/index.js
@@ -72,7 +72,7 @@ const SettingForm = ({createVisualization}) => {
     <div className='SettingForm'>
       <div className='form-group'>
         <Select
-          label="Number of Processors"
+          label="Number of Processes"
           value={processorsCount}
           options={['2', '3', '4', '5', '6', '7', '8', '9', '10']}
           enhanced
